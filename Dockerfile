a671df1945092e07cd98998c61bf416f3147d9a9
Maximilian Hess <max22h@icloud.com>
2023-09-07 14:38:32 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Use the official Python image from the Docker Hub
+FROM python:3.8-slim-buster
+
+# Set the working directory to /app
+WORKDIR /app
+
+# Copy the current directory contents into the container at /app
+COPY . /app
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir flask neo4j py2neo
+
+# Make port 5000 available to the world outside this container
+EXPOSE 5000
+
+# Define environment variable
+ENV NAME World
+
+# Run app.py when the container launches
+CMD ["python", "app.py"]
+
---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, redirect, url_for
+from py2neo import Graph
+
+app = Flask(__name__)
+
+graph = Graph("bolt://neo4j:7687", auth=("neo4j", "test"))
+
+@app.route("/")
+def home():
+    results = graph.run("MATCH (n)-[r]->(m) RETURN n, r, m").data()
+    return render_template("index.html", results=results)
+
+@app.route("/insert_data", methods=['POST'])
+def insert_data():
+    person = request.form['person']
+    connections = request.form.getlist('connection')
+    connection_names = request.form.getlist('connection_name')
+    graph.run(f"CREATE (n:Person {{name: '{person}'}})")
+    for conn, conn_name in zip(connections, connection_names):
+        graph.run(f"MATCH (a:Person {{name: '{person}'}}), (b:Person {{name: '{conn}'}}) CREATE (a)-[:{conn_name}]->(b)")
+    return redirect(url_for('home'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
